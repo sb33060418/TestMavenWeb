5d76203d34392a274fe5d06fcef7a8cbdee43fbc
sb33060418 <sb33060418@163.com>
2016-01-01 02:10:44 +0800

    初次提交

---
+++ src/main/java/com/sunbin/test/main/Test.java
@@ -0,0 +1,20 @@
+package com.sunbin.test.main;
+
+
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloWorld");
+	}
+	
+	public String test(){
+		System.out.println("test");
+		return "test";
+	}
+
+}
